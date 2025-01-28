9c0f37fe6ce0ad19ce13f02720165015bb534f39
nico <nicosena10@gmail.com>
2025-01-28 12:21:45 -0300

    terminado

---
+++ README.md
@@ -0,0 +1,25 @@
+# Mi Minijuego
+===============
+
+### Objetivo del juego
+-------------------------
+
+🎮 Adivinar un numero secreto en 3, 6 o 9 intentos, dependiendo de la dificultad elegida. Interfaz super intuitiva con mensajes de guía para el jugador. Ofrece pistas
+
+**Controles**:
+
+Teclado númerico, mouse
+
+### Cómo jugar
+--------------
+
+1. **Iniciar el juego**: Abre el siguiente enlace: 
+2. **Seleccionar el nivel**: Selecciona el nivel que deseas jugar introductiendo el número correspondiente. 1-facil, 2-normal, 3-dificil
+3. **Jugar**: Introduce los numeros
+
+### Créditos
+---------
+
+* **Desarrollador**: Sena, Nicolás Ariel.
+* **Fecha de lanzamiento**: 28/01/2025.
+* **Tiempo de desarrollo**: 3 horas.
---
+++ app.js
@@ -0,0 +1,58 @@
+let holaMundo = alert("Bienvenido al jueguito!");
+let numeroMax = 0;
+let maximoIntentos = 3;
+// math.random genera numeros que pertenecen al intervalo [0,1)
+let opcion = parseInt(
+  prompt(
+    "Escribe 1 para jugar en modo fácil, 2 para dificil, 3 muy dificil, otro para salir: "
+  )
+);
+switch (opcion) {
+  case 1:
+    numeroMax = 10;
+    break;
+  case 2:
+    (numeroMax = 30), (maximoIntentos *= 2);
+    break;
+  case 3:
+    (numeroMax = 50), (maximoIntentos *= 3);
+    break;
+  default:
+    break;
+}
+let numeroSecreto = Math.floor(Math.random() * numeroMax) + 1;
+let numeroUsuario = 0;
+let intentos = 0;
+let intentosRestantes = maximoIntentos;
+//let palabraIntento = "vez";
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = parseInt(
+    prompt(`Me indicas un numero entre 1 y ${numeroMax} por favor: `)
+  );
+  intentos++;
+  intentosRestantes--;
+  if (numeroUsuario == numeroSecreto) {
+    // {condicion} ? si es true [hace esto] : si es false [hace esto]
+    alert(
+      `Acertaste, el numero es: ${numeroSecreto}. Lo lograste en ${intentos} ${
+        intentos == 1 ? "vez" : "veces"
+      }`
+    );
+  } else {
+    alert(
+      `No acertaste el numero :( te quedan ${intentosRestantes} ${(intentosRestantes ==
+        1 ? "intento" : "intentos")}`
+    );
+    if (numeroUsuario < numeroSecreto) {
+      alert("El numero secreto es mayor!");
+    } else {
+      alert("El numero secreto es menor!");
+    }
+  }
+  if (intentos == maximoIntentos) {
+    alert(
+      `Has agotado tus ${maximoIntentos} intentos, el numero secreto era: ${numeroSecreto}`
+    );
+    break;
+  }
+}
